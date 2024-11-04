a21f20fc710541997ee4cc772a1c9f55d86d3d48
Muhammad-Ali-Asghar-Rizwan <programingmaster22@gmail.com>
2024-11-04 10:38:00 +0500

    UPDATE

--- src/app/component/Data.tsx
+++ src/app/component/Data.tsx
@@ -1,5 +1,5 @@
-import  Image from "next/image"
-import  React from "react"
+import  Image from "next/image";
+import  React from "react";
 import  { FaArrowRight } from "react-icons/fa6";
 
 
@@ -42,4 +42,4 @@ const Data = () => {
   )
 }
 
-export default Data
+export default Data;
